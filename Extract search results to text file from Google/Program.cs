e1d528d7655e4842d16801d01069aa8efbbe0e49
vladislav.tsvek <vladislav.tsvek@V-TSVEK>
2016-09-09 09:55:52 +0300

    Add project files.

---
+++ Extract search results to text file from Google/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Extract_search_results_to_text_file_from_Google
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
