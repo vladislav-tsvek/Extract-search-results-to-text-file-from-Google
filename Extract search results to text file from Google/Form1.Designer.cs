037de8810b6421fc0515f4c90a07e88f3944462d
vladislav.tsvek <vladislav.tsvek@teamdev.com>
2016-09-09 12:08:18 +0300

    Extracts the result of search from Google to text file

--- Extract search results to text file from Google/Form1.Designer.cs
+++ Extract search results to text file from Google/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripAddress = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripGoogleSearchToFile = new System.Windows.Forms.ToolStripButton();
+            this.browserView = new DotNetBrowser.WinForms.WinFormsBrowserView();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripAddress,
+            this.toolStripGoogleSearchToFile});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(859, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(77, 22);
+            this.toolStripLabel1.Text = "Enter address";
+            // 
+            // toolStripAddress
+            // 
+            this.toolStripAddress.Name = "toolStripAddress";
+            this.toolStripAddress.Size = new System.Drawing.Size(500, 25);
+            this.toolStripAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripAddress_KeyDown);
+            // 
+            // toolStripGoogleSearchToFile
+            // 
+            this.toolStripGoogleSearchToFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripGoogleSearchToFile.Image = ((System.Drawing.Image)(resources.GetObject("toolStripGoogleSearchToFile.Image")));
+            this.toolStripGoogleSearchToFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripGoogleSearchToFile.Name = "toolStripGoogleSearchToFile";
+            this.toolStripGoogleSearchToFile.Size = new System.Drawing.Size(119, 22);
+            this.toolStripGoogleSearchToFile.Text = "Google search to file";
+            this.toolStripGoogleSearchToFile.Click += new System.EventHandler(this.toolStripGoogleSearchToFile_Click);
+            // 
+            // browserView
+            // 
+            this.browserView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browserView.Location = new System.Drawing.Point(0, 25);
+            this.browserView.Name = "browserView";
+            this.browserView.Size = new System.Drawing.Size(859, 382);
+            this.browserView.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 407);
+            this.Controls.Add(this.browserView);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox toolStripAddress;
+        private System.Windows.Forms.ToolStripButton toolStripGoogleSearchToFile;
+        private DotNetBrowser.WinForms.WinFormsBrowserView browserView;
     }
 }
 
--- Extract search results to text file from Google/Form1.cs
+++ Extract search results to text file from Google/Form1.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using DotNetBrowser;
+using DotNetBrowser.DOM;
+using DotNetBrowser.Events;
+using DotNetBrowser.WinForms;
+using DotNetBrowser.WPF;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +20,145 @@ namespace Extract_search_results_to_text_file_from_Google
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            browserView.Browser.LoadURL("http://www.google.com");
+
+            ComplexPageLoad();
+
+            this.Text = browserView.Browser.Title;
+            toolStripAddress.Text = browserView.Browser.URL.ToString();
+        }
+
+        private void toolStripAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ComplexPageLoad();
+                browserView.Browser.LoadURL(toolStripAddress.Text.ToString());
+                this.Text = browserView.Browser.Title;
+                toolStripAddress.Text = browserView.Browser.URL.ToString();
+            }
+        }
+
+        private void ComplexPageLoad()
+        {
+            ManualResetEvent resetEvent = new ManualResetEvent(false);
+            FinishLoadingFrameHandler listener = new FinishLoadingFrameHandler((object sender, FinishLoadingEventArgs e) =>
+            {
+                if (e.IsMainFrame)
+                {
+                    resetEvent.Set();
+                }
+            });
+            browserView.Browser.FinishLoadingFrameEvent += listener;
+            try
+            {
+                browserView.Browser.LoadURL(toolStripAddress.Text.ToString());
+                resetEvent.WaitOne(new TimeSpan(0, 0, 45));
+            }
+            finally
+            {
+                browserView.Browser.FinishLoadingFrameEvent -= listener;
+            }
+        }
+
+        private void toolStripGoogleSearchToFile_Click(object sender, EventArgs e)
+        {
+            //Only for Google search results
+
+            DOMDocument document = browserView.Browser.GetDocument();
+            string allTextToFile = "";
+            string searchWord = "";
+
+            try
+            {
+                searchWord = document.GetElementByClassName("q qs").GetAttribute("href").Split('&')[0].ToString();
+            }
+            catch
+            {
+                MessageBox.Show("There is no info to parse", "Error!");
+            }
+
+
+            if (searchWord.Contains("/search?q="))
+            {
+                ManualResetEvent resetEvent = new ManualResetEvent(false);
+                FinishLoadingFrameHandler listener = new FinishLoadingFrameHandler((object sender1, FinishLoadingEventArgs e1) =>
+                {
+                    if (e1.IsMainFrame)
+                    {
+                        resetEvent.Set();
+                    }
+                });
+                browserView.Browser.FinishLoadingFrameEvent += listener;
+
+                try
+                {
+                    browserView.Browser.LoadURL("https://www.google.com.ua" + searchWord);
+                    resetEvent.WaitOne(new TimeSpan(0, 0, 45));
+                }
+                finally
+                {
+                    browserView.Browser.FinishLoadingFrameEvent -= listener;
+                }
+
+                // Return address of Web page
+                toolStripAddress.Text = browserView.Browser.URL.ToString();
+                this.Text = browserView.Browser.Title;
+
+                int countDiv = document.GetElementsByTagName("div").Count;
+                int countH3 = 0;
+
+                //Quantity of <h3> tags in <div> tags
+                for (int i = 0; i < countDiv; i++)
+                {
+                    int tmpH3 = document.GetElementsByTagName("div")[i].GetElementsByTagName("h3").Count;
+                    countH3 += tmpH3;
+                }
+
+                //Search for <a> tags in <h3>
+                for (int i = 0; i < countH3; i++)
+                {
+                    string text = "";
+                    try
+                    {
+                        text = document.GetElementsByTagName("h3")[i].GetElementByTagName("a").InnerText.ToString();
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                                                           
+                    //Make string for add info to file
+                    string textToFile = "# " + (i + 1) + Environment.NewLine +
+                       "name =  " + text + Environment.NewLine +
+                       "hyperlink = " + document.GetElementsByTagName("h3")[i].GetElementByTagName("a").GetAttribute("href") + Environment.NewLine;
+
+                    allTextToFile += textToFile + Environment.NewLine;
+                }
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "TXT files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+                saveFileDialog.RestoreDirectory = true;
+
+                //Choose path and name to save the file
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Format name of file
+                    string txtFileName = saveFileDialog.FileName.ToString();
+                    //string weq = saveFileDialog.
+                    File.WriteAllText(txtFileName, allTextToFile);
+                }
+            }
         }
     }
 }
